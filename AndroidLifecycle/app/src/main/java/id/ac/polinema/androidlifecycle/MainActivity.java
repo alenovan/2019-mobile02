1605dba385b228babc5c3dbd82ea67bc92ced3b6
alenovan <novannanega@gmail.com>
2019-09-03 14:32:08 +0700

    Tugas Alenovan add oveeride

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -7,56 +7,55 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	// TODO: tambahkan callback onStart() di sini
+    // TODO: tambahkan callback onStart() di sini
 
-	@Override
-	protected void onStart() {
-		Toast.makeText(this,"App On Start",Toast.LENGTH_SHORT).show();
-		super.onStart();
-	}
+    @Override
+    protected void onStart() {
+        Toast.makeText(this, "App On Start", Toast.LENGTH_SHORT).show();
+        super.onStart();
+    }
 
-	// TODO: tambahkan callback onStop() di sini
+    // TODO: tambahkan callback onStop() di sini
 
-	@Override
-	protected void onStop() {
-		Toast.makeText(this,"App On Stop",Toast.LENGTH_SHORT).show();
-		super.onStop();
-	}
+    @Override
+    protected void onStop() {
+        Toast.makeText(this, "App On Stop", Toast.LENGTH_SHORT).show();
+        super.onStop();
+    }
 
-	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
-
-
-	@Override
-	protected void onDestroy() {
-		Toast.makeText(this,"App On Destroy",Toast.LENGTH_SHORT).show();
-		super.onDestroy();
-	}
-
-	@Override
-	protected void onPause() {
-		Toast.makeText(this,"App On Pause",Toast.LENGTH_SHORT).show();
-		super.onPause();
-	}
-
-	@Override
-	protected void onRestart() {
-		Toast.makeText(this,"App On Restart",Toast.LENGTH_SHORT).show();
-		super.onRestart();
-	}
+    // TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
 
 
-	@Override
-	protected void onResume() {
-		Toast.makeText(this,"App On Resume",Toast.LENGTH_SHORT).show();
-		super.onResume();
-	}
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "App On Destroy", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
 
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "App On Pause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        Toast.makeText(this, "App On Restart", Toast.LENGTH_SHORT).show();
+        super.onRestart();
+    }
+
+
+    @Override
+    protected void onResume() {
+        Toast.makeText(this, "App On Resume", Toast.LENGTH_SHORT).show();
+        super.onResume();
+    }
 
 
 }
